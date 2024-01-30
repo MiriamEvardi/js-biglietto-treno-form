acd584b76a1717f71ee77704e667bf406011251b
MiriamEvardi <miriamevardi97@gmail.com>
2024-01-30 17:09:38 +0100

    JS done

--- js/script.js
+++ js/script.js
@@ -1,39 +1,76 @@
 const kmElement = document.querySelector("#input-km");
-
 const ageElement = document.querySelector("#input-age");
-
 const startButtonElement = document.querySelector("#start");
 
 console.log(startButtonElement)
+
+
+let km;
+let age;
+
+
 
 let kmPrice = 0.21;
 
 let discountedPrice = kmPrice;
 
+
 startButtonElement.addEventListener("click",
     function () {
-        if (!isNaN(ageElement) && 0 < ageElement && 100 > ageElement && Number.isInteger(ageElement) && Number.isInteger(kmElement) && !isNaN(kmElement) && kmElement > 0) {
 
-            if (ageElement < 18) {
-                let discount = 0.2;
-
-                document.getElementById('discount').innerHTML = "Hai ricevuto uno sconto del 20%";
-                discountedPrice = kmElement * (1 - discount);
+        age = ageElement.value;
+        km = kmElement.value;
 
 
-            } else if (ageElement > 64) {
-                let discount = 0.4;
 
-                document.getElementById('discount').innerHTML = "Hai ricevuto uno sconto del 40%";
-                discountedPrice = kmElement * (1 - discount);
+        let errorHappened = false;
+        if (errorHappened == false) {
+
+            if (isNaN(age) || age <= 0 || age > 105) {
+                document.querySelector("#age-error").innerHTML = "Valore errato, inserire un valore valido";
+                errorHappened = true;
+            } else {
+                document.querySelector("#age-error").innerHTML = "";
             }
-
-            let totalCost = kmElement * discountedPrice;
-            let lastPrice = totalCost.toFixed(2);
-
-            document.getElementById('train-ticket').innerHTML = lastPrice + ' €';
-            console.log("totalCost");
-
         }
+
+
+        if (isNaN(km) || km < 1 || km == 0) {
+            document.querySelector("#km-error").innerHTML = "Valore errato, inserire un valore valido";
+
+            errorHappened = true;
+        }
+
+
+
+
+        if (age < 18) {
+            let discount = 0.2;
+
+            document.getElementById('discount').innerHTML = "Hai ricevuto uno sconto del 20%";
+            discountedPrice = kmPrice * (1 - discount);
+
+        } else if (age > 64) {
+            let discount = 0.4;
+
+            document.getElementById('discount').innerHTML = "Hai ricevuto uno sconto del 40%";
+            discountedPrice = kmPrice * (1 - discount);
+
+        } else {
+            document.getElementById('discount').innerHTML = "";
+            discountedPrice = kmPrice;
+        }
+
+        let totalCost = km * discountedPrice;
+        let lastPrice = totalCost.toFixed(2);
+
+        document.getElementById('train-ticket').innerHTML = lastPrice + ' €';
+        console.log("totalCost");
+
+
     }
 )
+
+if (!errorHappened) {
+    document.getElementById("result").innerHTML = "Il prezzo del tuo biglietto è di € " + finalPrice.toFixed(2) + "<br>" + discountMessage;
+}
